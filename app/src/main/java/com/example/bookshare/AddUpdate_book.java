99cefb30c44d56224d47e65b10596232a77fb368
Ashish Gujral <ashishgujral@Ashishs-MacBook-Pro.local>
2022-03-09 13:37:59 -0800

    Initial commit

---
+++ app/src/main/java/com/example/bookshare/AddUpdate_book.java
@@ -0,0 +1,14 @@
+package com.example.bookshare;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddUpdate_book extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_update_book);
+    }
+}
